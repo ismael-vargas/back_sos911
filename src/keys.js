eaabb520470ede2481f2c3e2df545e922e783076
ismael-vargas <141758005+ismael-vargas@users.noreply.github.com>
2025-06-24 11:28:38 -0500

    keys sos911 cambios

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
-const MYSQLHOST = 'localhost';
-const MYSQLUSER = 'root';
-const MYSQLPASSWORD = '';
+const MYSQLHOST = '31.97.42.126';
+const MYSQLUSER = 'linkear';
+const MYSQLPASSWORD = '0987021692@Rj';
 const MYSQLDATABASE = 'sos911';
 const MYSQLPORT = '3306'; // Puerto de la base de datos
 const MYSQL_URI = process.env.MYSQL_URI ?? ''; // URI de conexión a la base de datos MySQL
