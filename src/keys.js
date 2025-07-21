9619923528ad590defb89248b4a5d900ed733b55
ismael-vargas <141758005+ismael-vargas@users.noreply.github.com>
2025-07-21 12:43:30 -0500

    mongo puerto

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,7 @@ const MYSQLPASSWORD = '0987021692@Rj';
 const MYSQLDATABASE = 'sos911';
 const MYSQLPORT = '3306';
 const MYSQL_URI = process.env.MYSQL_URI ?? '';
-const MONGO_URI = 'mongodb://localhost:27017/sos_mongo';
+const MONGO_URI = 'mongodb://linkear:0987021692%40Rj@31.97.42.126:27017/sos911?authSource=sos911';
 
 module.exports = {
     MYSQLHOST,
